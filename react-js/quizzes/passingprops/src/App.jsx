1dbbd8e7298798ab3b6a689bc2f23fe31c569790
Nia Wright <111546789+nianokia@users.noreply.github.com>
2024-10-16 12:33:16 -0400

    Add a comment explaining how the app functions and why props are passed to and from
    Document how the app works and how the data moves from one component to another.

--- react-js/quizzes/passingprops/src/App.jsx
+++ react-js/quizzes/passingprops/src/App.jsx
@@ -10,6 +10,11 @@ function App() {
     setUserEmail(data);
   };
 
+  // DON'T DELETE - The app creates a userEmail state that is updated in the onSendData prop of the form component. Once the 
+  // user clicks "Send", the userEmail state is updated and sent back to the parent. The parent then sends this userEmail to
+  // the message component as an email prop. With this, the message component can conditionally render a "Thank you" message
+  // if the user successfully enters an email.
+  
   return (
     <>
       <div>
