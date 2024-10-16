315c523f71f359332e6955aec64d43a13ce131b5
Nia Wright <111546789+nianokia@users.noreply.github.com>
2024-10-16 12:14:34 -0400

    Add a comment explaining why App.jsx passes the email prop to message.jsx
    Explanation of why it's important to pass the email prop to the message component for the app to function properly.

--- react-js/quizzes/passingprops/src/components/message.jsx
+++ react-js/quizzes/passingprops/src/components/message.jsx
@@ -2,6 +2,8 @@ function Message({email}) {
   
     return (
       <div>
+        {/* DON'T DELETE - It's important for the parent (App.jsx) to send the email prop to the message component so that it can 
+        conditionally render a message if the user succesfully enters an email in the form component */}
         {!email ? null : <p> Thank you for submiting your email: {email} </p>}
       </div>
     );
