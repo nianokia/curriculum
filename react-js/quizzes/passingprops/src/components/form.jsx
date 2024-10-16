269cd0e28f2ecd1ff4af4b203a44f1bcd11f1c6c
Nia Wright <111546789+nianokia@users.noreply.github.com>
2024-10-16 12:07:29 -0400

    Add comment to explain form.jsx passing data back to App.jsx
    Explanation of why it's important that the form component is passing data back to the parent (App.jsx).

--- react-js/quizzes/passingprops/src/components/form.jsx
+++ react-js/quizzes/passingprops/src/components/form.jsx
@@ -7,6 +7,9 @@ function Form({ onSendData }) {
     setInputValue(e.target.value);
   };
 
+  // DON'T DELETE - the form component will send the final inputValue back to the parent component (App.jsx) when the user clicks "Send".
+  // This is important so that the message component knows if the user entered an email or not to display a Thank you message.
+  // The onSendData prop is the handleReceiveData function in the parent component. It was just renamed in the child component
   const handleSendData = () => {
     onSendData(inputValue);
   };
